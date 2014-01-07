eb87db7649c7e9c96481845c3a81d8b2452bbbe6
aviandri <aviandri@gmail.com>
2014-01-07 14:33:05 +0700

    Change field limit for post-tests

---
+++ db/migrate/20140107073132_change_post_test_fields_sizes.rb
@@ -0,0 +1,13 @@
+class ChangePostTestFieldsSizes < ActiveRecord::Migration
+  def up
+  	change_column :post_tests, :question, :string, :limit => nil
+  	change_column :post_tests, :answer_one, :string, :limit => nil
+  	change_column :post_tests, :answer_two, :string, :limit => nil
+  	change_column :post_tests, :answer_three, :string, :limit => nil
+  	change_column :post_tests, :answer_four, :string, :limit => nil
+  	change_column :post_tests, :answer_five, :string, :limit => nil
+  end
+
+  def down
+  end
+end
